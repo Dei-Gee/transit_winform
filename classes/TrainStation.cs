d4d3163905e511e3d548eb2e89794db616c3affb
Dei-Gee <deji2109@gmail.com>
2019-09-30 10:27:00 -0700

    trainstation class added

---
+++ classes/TrainStation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transit_Winform.classes
+{
+    class TrainStation
+    {
+        private string _name;
+        private string _line;
+        private bool _is_changeover;
+
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public string Line
+        {
+            get
+            {
+                return this._line;
+            }
+
+            set
+            {
+                this._line = value;
+            }
+        }
+
+        public bool IsChangeOver
+        {
+            get
+            {
+                return this._is_changeover;
+            }
+
+            set
+            {
+                this._is_changeover = value;
+            }
+        }
+    }
+}
